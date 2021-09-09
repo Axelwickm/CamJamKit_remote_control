c7e95fa6ccd5570e7fd0d1291c27cd7d3ac6ad64
Axelwickm <axelwickm@gmail.com>
2021-09-09 15:18:47 +0200

    Hosting both http and websocket

--- remote_control_server.py
+++ remote_control_server.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
+import aiohttp
+from aiohttp import web, WSCloseCode
 import asyncio
-import websockets
 
-from gpiozero import CamJamKitRobot
+#from gpiozero import CamJamKitRobot
 
-robot = CamJamKitRobot()
+#robot = CamJamKitRobot()
 
 
 state = {
@@ -15,29 +16,59 @@ state = {
     "right": 0
 }
 
-async def drive(websocket, path):
-    global state, robot
-    async for message in websocket:
-        #print("Command: "+message)
-        command = message.split()
-        if command[1] == "press":
-            state[command[0]] += 1
-        elif command[1] == "release":
-            state[command[0]] -= 1
-        
-        left = right = 0.0
-        left += - state["up"] + state["down"] + state["left"] - state["right"]
-        right += - state["up"] + state["down"] + state["right"] - state["left"]
+def drive(command):
+    global state
+    command = message.split()
+    if command[1] == "press":
+        state[command[0]] += 1
+    elif command[1] == "release":
+        state[command[0]] -= 1
+    
+    left = right = 0.0
+    left += - state["up"] + state["down"] + state["left"] - state["right"]
+    right += - state["up"] + state["down"] + state["right"] - state["left"]
 
-        left = min(max(left, -1), 1)
-        right = min(max(right, -1), 1)
-
-        robot.value = (left, right)
-
-async def main():
-    async with websockets.serve(drive, port=2155):
-        await asyncio.Future()  # run forever
-
-asyncio.run(main())
+    left = min(max(left, -1), 1)
+    right = min(max(right, -1), 1)
+    print(left, right)
+    #robot.value = (left, right)
 
 
+async def http_handler(request):
+    return web.FileResponse("index.html")
+
+
+async def websocket_handler(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    async for msg in ws:
+        if msg.type == aiohttp.WSMsgType.TEXT:
+            drive(msg.data)
+        elif msg.type == aiohttp.WSMsgType.ERROR:
+            print('ws connection closed with exception %s' % ws.exception())
+
+    return ws
+
+
+def create_runner():
+    app = web.Application()
+    app.add_routes([
+        web.get("/", http_handler),
+        web.get('/ws', websocket_handler),
+    ])
+    app.router.add_static("/", ".")
+    return web.AppRunner(app)
+
+
+async def start_server(host="127.0.0.1", port=80):
+    runner = create_runner()
+    await runner.setup()
+    site = web.TCPSite(runner, host, port)
+    await site.start()
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(start_server())
+    loop.run_forever()
